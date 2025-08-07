307cdce969fbb3076c67296cb9b2bea9c3ab49c0
Zhankj007 <128675867+Zhankj007@users.noreply.github.com>
2025-08-07 16:41:34 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -2,7 +2,7 @@
 export default function handler(request, response) {
     response.setHeader('Content-Type', 'application/json; charset=utf-8');
     response.status(200).json({ 
-        message: "API服务运行正常！可以连接到这里。",
+        message: "API服务运行正常！可以连接到这。",
         timestamp: new Date().toISOString() 
     });
 }
